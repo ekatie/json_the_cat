09a5fa851da486e2448cb809a1d04ff3a0886f50
Katie K <katiekucsera@gmail.com>
2023-09-22 16:25:38 -0400

    Refactor commenting for clarity

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,15 @@
 const request = require('request');
 
+/**
+ * This function that takes in a breedname from the command line and prints out a cat breed description based on user input.
+ * @param {string} breedName - String input using CLI arguments
+ * @param {function} callback - Function that takes the error and description generated as parameters.
+ * Returns data to the callback function, which prints the data to the console.
+ */
+
 const fetchBreedDescription = function(breedName, callback) {
 
+  // Generate URL based on CLI args
   const url = 'https://api.thecccatapi.com/v1/breeds/search?q=' + breedName;
 
   request(url, (error, response, body) => {
