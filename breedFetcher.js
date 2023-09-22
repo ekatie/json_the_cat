b5a6b2809fb94a2e3c02d2281c32d8b2560b228b
Katie K <katiekucsera@gmail.com>
2023-09-22 14:55:02 -0400

    Request to url and parse data

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const args = process.argv;
+
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + args[2];
+
+
+request(url, (error, response, body) => {
+
+  if (error || response.statusCode !== 200) {
+    throw Error('An error occured while accessing the URL', error);
+  } else {
+
+    const data = JSON.parse(body);
+    console.log(typeof data);
+    // console.log(data[0]);
+  }
+});
