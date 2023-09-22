6af53fb5166b2bfc8a92d8326c20c7ea70905d00
Katie K <katiekucsera@gmail.com>
2023-09-22 15:11:57 -0400

    Refactor commenting for clarity

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,20 @@ const args = process.argv;
 
 const url = 'https://api.thecccatapi.com/v1/breeds/search?q=' + args[2];
 
-
 request(url, (error, response, body) => {
 
+  // Check for error with URL
   if (error) {
     throw Error('Error: Invalid URL', error);
-  } else if (response.statusCode !== 200) {
+  }
+
+  // Check for response failure
+  else if (response.statusCode !== 200) {
     throw Error('Request failed:', response.statusCode);
-  } else {
+  }
+
+  // Print description to console
+  else {
     const data = JSON.parse(body);
     let catDescription = data[0].description;
     if (catDescription === undefined) {
@@ -18,5 +24,4 @@ request(url, (error, response, body) => {
     }
     console.log(catDescription);
   }
-
 });
